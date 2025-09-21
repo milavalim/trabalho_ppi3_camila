c87033e18c940ecee621df50ae86ea1bd00d8ba0
Camila Valim Dias <camila.valim2004@gmail.com>
2025-09-21 15:34:42 -0300

    Fix: Add [titulo] to fillable property to allow mass assignment on [App\Models\Receita]. Corrigido em App > Models > Receita

--- app/Models/Receita.php
+++ app/Models/Receita.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Receita extends Model
 {
-    //
+    use HasFactory;
+
+    // Libera os campos que podem ser salvos via create() ou update()
+    protected $fillable = [
+        'titulo',
+        'ingredientes',
+        'descricao',
+        'arquivo',
+        'user_id',
+    ];
 }
